21bb37d883700b7ad85f068fda054d13ae680d48
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-20 18:37:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /use/bin/evn node
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("invalid");
+}
